3aa65cf9e36a2cc756c09698b525b03ffd925fa9
Mehmet Emre Atasever <m.emre.atasever@gmail.com>
2023-09-28 16:07:32 +0300

    add DoShread
    - use /dev/urandom as random data generator
    - write random data to the file 3 times

--- shred/shred.go
+++ shred/shred.go
@@ -2,16 +2,74 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+)
+
+// 1K chunk size
+const (
+	chunkSize  = 1024
+	shredCount = 3
 )
 
 func Shred(filename string) error {
 	// Check if the file exists
-	if _, err := os.Stat(filename); os.IsNotExist(err) {
+	fileInfo, err := os.Stat(filename)
+	if err != nil {
 		fmt.Printf("File %s not found\n", filename)
 		return err
 	}
 
-	fmt.Printf("File %s found\n", filename)
+	fileSize := fileInfo.Size()
+
+	fmt.Printf("File %s found (%d bytes)\n", filename, fileSize)
+
+	for i := 1; i <= shredCount; i++ {
+		if err := DoShred(filename, fileSize); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// DoShred is a function that shreds a file with random data.
+func DoShred(filename string, fileSize int64) error {
+	file, err := os.OpenFile(filename, os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	randomFile, err := os.Open("/dev/urandom")
+	if err != nil {
+		return err
+	}
+	defer randomFile.Close()
+	randomData := make([]byte, chunkSize)
+
+	for offset := int64(0); offset < fileSize; offset += chunkSize {
+		// Generate random data for the chunk
+		_, err := io.ReadFull(randomFile, randomData)
+		if err != nil {
+			return err
+		}
+
+		// Seek to the current offset and write the random data
+		_, err = file.Seek(offset, io.SeekStart)
+		if err != nil {
+			return err
+		}
+
+		_, err = file.Write(randomData)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Sync and truncate the file to ensure data is written
+	if err := file.Sync(); err != nil {
+		return err
+	}
+
 	return nil
 }
--- shred/shred_test.go
+++ shred/shred_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"os"
 	"testing"
 )
@@ -13,6 +14,7 @@ func TestShredFileNotFound(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	// Remvove the file
 	os.Remove(tmpFile.Name())
 
 	// Call Shred with a non-existent filename
@@ -24,7 +26,7 @@ func TestShredFileNotFound(t *testing.T) {
 	}
 }
 
-// given a filename that was not exists
+// given a filename that was created before
 // then expected Shred returns success
 func TestShredFileFound(t *testing.T) {
 	// Create a temporary test file and get its name
@@ -41,5 +43,76 @@ func TestShredFileFound(t *testing.T) {
 	// Check if the error is nil (file found)
 	if err != nil {
 		t.Errorf("Expected no error, but got %v for file %s", err, tmpFile.Name())
+	}
+}
+
+// given a filename that was not exists
+// then expected DoShred cannot shred the file
+func TestDoShredFileNotExists(t *testing.T) {
+	// Create a temporary test file and get its name
+	tmpFile, err := os.CreateTemp("", "shred_example_")
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Remove the file
+	os.Remove(tmpFile.Name())
+
+	// Call Shred with the existing filename
+	err = DoShred(tmpFile.Name(), 34343)
+
+	// Check if the error is not nil (file not found)
+	if err == nil {
+		t.Errorf("Expected error, but got nil for file %s", tmpFile.Name())
+	}
+}
+
+// given a filename that was created before
+// then expected DoShred shreds the file content
+func TestDoShred(t *testing.T) {
+	// Create a temporary test file and get its name
+	tmpFile, err := os.CreateTemp("", "shred_example_")
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Remove the temporary file when test finished
+	defer os.Remove(tmpFile.Name()) // Remove the temporary file
+
+	// Populate the file with random data (4K)
+	randomData := make([]byte, 4*1024)
+	randomFile, err := os.Open("/dev/urandom")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer randomFile.Close()
+	_, err = io.ReadFull(randomFile, randomData)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = tmpFile.Write(randomData)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Save the original content
+	originalContent, err := os.ReadFile(tmpFile.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Call DoShred on the file
+	err = DoShred(tmpFile.Name(), int64(len(randomData)))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Read the content after shredding
+	shreddedContent, err := os.ReadFile(tmpFile.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Compare the content before and after shredding
+	if string(originalContent) == string(shreddedContent) {
+		t.Error("File content is not different after shredding")
 	}
 }
